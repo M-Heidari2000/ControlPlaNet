7331b845e61902c6395d586d2dda401984bfefe4
Milad Heidari <milad.h9999@gmail.com>
2026-03-12 19:13:40 -0700

    Add: action collection using learned ppo model

--- envs/utils.py
+++ envs/utils.py
@@ -1,9 +1,10 @@
 import minari
 import numpy as np
 from tqdm import tqdm
+from stable_baselines3 import PPO
 import gymnasium as gym
 from pathlib import Path
-from typing import Union
+from typing import Union, List
 from minari.dataset.minari_storage import MinariStorage
 from minari.data_collector.episode_buffer import EpisodeBuffer
 
@@ -24,8 +25,8 @@ MinariStorage.get_size = patched_get_size
 def collect_data(
     env: gym.Env,
     data_dir: Union[str, Path],
+    ppo_steps_list: List[int],
     num_episodes: int = 100,
-    action_repeat: int = 1,
 ) -> MinariStorage:
 
     data_dir = Path(data_dir)
@@ -38,46 +39,43 @@ def collect_data(
         data_format="hdf5",
     )
 
-    # random target but fixed across the episodes
-    for _ in tqdm(range(num_episodes)):
-        obs, info = env.reset()
+    for ppo_steps in ppo_steps_list:
 
-        episode = EpisodeBuffer(
-            id=None,
-            options=None,
-            observations=[],
-            actions=[],
-            rewards=[],
-            terminations=[],
-            truncations=[],
-            infos={"state": []},
-        )
+        # train a ppo model for the specified timesteps
+        model = PPO("MlpPolicy", env=env)
+        model.learn(total_timesteps=ppo_steps)
 
-        done = False
-        action_counter = 0
-        action = env.action_space.sample()
+        # collect data using the trained ppo model
+        for _ in tqdm(range(num_episodes)):
+            episode = EpisodeBuffer(
+                id=None,
+                options=None,
+                observations=[],
+                actions=[],
+                rewards=[],
+                terminations=[],
+                truncations=[],
+                infos={"state": []},
+            )
 
-        while not done:
-            
-            if action_counter >= action_repeat:
-                action = env.action_space.sample()
-                action_counter = 0
-            
-            next_obs, reward, terminated, truncated, next_info = env.step(action=action)
+            obs, info = env.reset()
+            done = False
+            while not done:
+                action, _ = model.predict(obs, deterministic=True)
+                next_obs, reward, terminated, truncated, next_info = env.step(action=action)
 
-            episode.observations.append(np.array(obs, copy=True))
-            if "state" in info:
-                episode.infos["state"].append(np.array(info["state"], copy=True))
-            episode.actions.append(np.array(action, copy=True))
-            episode.rewards.append(float(reward))
-            episode.terminations.append(bool(terminated))
-            episode.truncations.append(bool(truncated))
+                episode.observations.append(np.array(obs, copy=True))
+                if "state" in info:
+                    episode.infos["state"].append(np.array(info["state"], copy=True))
+                episode.actions.append(np.array(action, copy=True))
+                episode.rewards.append(float(reward))
+                episode.terminations.append(bool(terminated))
+                episode.truncations.append(bool(truncated))
 
-            done = terminated or truncated
-            obs, info = next_obs, next_info
-            action_counter += 1
+                done = terminated or truncated
+                obs, info = next_obs, next_info
 
-        storage.update_episodes([episode])
+            storage.update_episodes([episode])
 
     # these are not important but minari needs when loading the storage
     storage.update_metadata({
